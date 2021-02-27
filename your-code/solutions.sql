c6a5da892b737ba9ae7ac24819a098e013af5700
maria <alvarezferreiro.maria@gmail.com>
2021-02-27 18:43:13 +0100

    pendiente bonus + ifnull

--- your-code/solutions.sql
+++ your-code/solutions.sql
@@ -3,22 +3,100 @@ USE `sakila`;
 SELECT COUNT(*) FROM `titleauthor`;
 #25 rows
 
-SELECT `authors.au_id` AS `AUTHOR ID`, `authors.au_lname` AS `LAST NAME`, 
-`authors.au_fname` AS `FIRST NAME`, `titles.title` AS `TITLE`, `publishers.pub_name` AS `PUBLISHER`
+-- challenge 1
+-- we need to create a subquery in order to count in a no-existing-table(newtable)
+SELECT COUNT(1) FROM 
+(SELECT authors.au_id AS `AUTHOR ID`, authors.au_lname AS `LAST NAME`, 
+authors.au_fname AS `FIRST NAME`, titles.title AS `TITLE`, publishers.pub_name AS `PUBLISHER`
 FROM 
-	`publishers`
+	`titles`
     JOIN
-    `titles`
+    `publishers`
     ON 
-    `publishers.pub_id`=`titles.pub_id`
+    publishers.pub_id=titles.pub_id
     JOIN 
     `titleauthor`
 	ON
-    `title.title_id`=`titleauthor.title_id`
+    titles.title_id=titleauthor.title_id
     JOIN
     `authors`
     ON
-    `authors.au_id`=`title.au_id`;
+    authors.au_id=titleauthor.au_id) AS newtable;
     
+-- challenge 2
+SELECT authors.au_id AS `AUTHOR ID`, authors.au_lname AS `LAST NAME`, 
+authors.au_fname AS `FIRST NAME`, publishers.pub_name AS `PUBLISHER`, COUNT(titles.title) AS `TITLE COUNT`
 
-#Error Code: 1054. Unknown column 'authors.au_id' in 'field list'
+FROM 
+	`titles`
+    JOIN
+    `publishers`
+    ON 
+    publishers.pub_id=titles.pub_id
+    JOIN 
+    `titleauthor`
+	ON
+    titles.title_id=titleauthor.title_id
+    JOIN
+    `authors`
+    ON
+    authors.au_id=titleauthor.au_id
+
+GROUP BY `AUTHOR ID`, `FIRST NAME`, `LAST NAME`,`PUBLISHER`
+ORDER BY `TITLE COUNT` DESC, `AUTHOR ID` DESC;
+
+-- challenge 3
+
+SELECT * FROM sales;
+SELECT * FROM titles;
+
+SELECT authors.au_id AS `AUTHOR ID`, authors.au_lname AS `LAST NAME`, 
+authors.au_fname AS `FIRST NAME`, SUM(sales.qty) AS `TOTAL`
+
+FROM 
+	`titles`
+    JOIN
+    `sales`
+    ON 
+    sales.title_id=titles.title_id
+    JOIN 
+    `titleauthor`
+	ON
+    titles.title_id=titleauthor.title_id
+    JOIN
+    `authors`
+    ON
+    authors.au_id=titleauthor.au_id
+
+-- TOTAL is the number of titles sold by authors, but in the "hint" it talks about the total profits, should we have in consideration also the price per title?
+GROUP BY `AUTHOR ID`, `FIRST NAME`, `LAST NAME`
+ORDER BY `TOTAL`DESC
+LIMIT 3;
+
+-- CHALLENGE 4
+
+SELECT authors.au_id AS `AUTHOR ID`, authors.au_lname AS `LAST NAME`, 
+authors.au_fname AS `FIRST NAME`, SUM(sales.qty) AS `TOTAL`, IFNULL(`TOTAL`,0)
+
+FROM 
+	`titles`
+    JOIN
+    `sales`
+    ON 
+    sales.title_id=titles.title_id
+    JOIN 
+    `titleauthor`
+	ON
+    titles.title_id=titleauthor.title_id
+    RIGHT JOIN
+    `authors`
+    ON
+    authors.au_id=titleauthor.au_id
+
+
+GROUP BY `AUTHOR ID`, `FIRST NAME`, `LAST NAME`
+ORDER BY `TOTAL`DESC;
+
+
+
+
